3997ec6e30852c456281d79782e7832e80e5c6bd
Fredericia <39918560+stevD9@users.noreply.github.com>
2018-09-28 15:34:49 +0300

    Hide navbar on scroll and COMPLETED

--- resources/js/main.js
+++ resources/js/main.js
@@ -8,4 +8,19 @@ $(document).ready(function(){
     pauseOnFocus: false,
     pauseOnHover: false
   });
+
+  var scrolled = 0;
+  $(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop - scrolled > 50) {
+      var navHeight = $('.navbar').css('height');
+      $('.navbar').animate({
+        top: '-' + navHeight}, 150);
+      scrolled = scrollTop;
+    } else if (scrolled - scrollTop > 50) {
+      $('.navbar').animate({
+        top: '0px'}, 150);
+      scrolled = scrollTop;
+    }
+  });
 });
